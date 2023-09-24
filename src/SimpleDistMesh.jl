9699ffc6dd4fd9a9f21b5381aa041c23d5f0016f
Alex <alex.nunn@pm.me>
2023-09-24 15:20:21 -0700

    Improve documentation

--- src/SimpleDistMesh.jl
+++ src/SimpleDistMesh.jl
@@ -6,13 +6,28 @@ Mesh generation using signed distance functions
 # Examples
 Create a mesh for a circular disk
 ```
-
-
+julia> mesh = Mesh(
+        Circle([0, 0], 1),  # d -- signed distance function
+        pt -> 1,            # h -- element size function
+        0.05,               # h0 -- initial element size
+        [-1 -1; 1, 1]       # bounds -- [x_min y_min; x_max y_max]
+    )
+Mesh(Matrix[... ], Matrix[...])
 ```
 
-Mesh for a square with a circular hole
+Mesh for a square `[-1, 1]×[-1, 1]` with a circular hole
 ```
-
+julia> d = setdiff(
+            Rect(-1, 1, -1, 1)),
+            Circle([0.0, 0.0], 1/2)
+        );
+julia> mesh = Mesh(
+        d,                  # d -- signed distance function
+        pt -> 1,            # h -- element size function
+        0.05,               # h0 -- initial element size
+        [-1 -1; 1, 1]       # bounds -- [x_min y_min; x_max y_max]
+    )
+Mesh(Matrix[... ], Matrix[...])
 ```
 """
 module SimpleDistMesh
