c4330ad217da1c7cfc4d88b62f98456c9371e5dc
Alex <alex.nunn@pm.me>
2023-09-24 15:26:08 -0700

    Fix formatting of documenation

--- src/SimpleDistMesh.jl
+++ src/SimpleDistMesh.jl
@@ -7,27 +7,25 @@ Mesh generation using signed distance functions
 Create a mesh for a circular disk
 ```
 julia> mesh = Mesh(
-        Circle([0, 0], 1),  # d -- signed distance function
-        pt -> 1,            # h -- element size function
-        0.05,               # h0 -- initial element size
-        [-1 -1; 1, 1]       # bounds -- [x_min y_min; x_max y_max]
-    )
-Mesh(Matrix[... ], Matrix[...])
+           Circle([0, 0], 1),  # d -- signed distance function
+           pt -> 1,            # h -- element size function
+           0.05,               # h0 -- initial element size
+           [-1 -1; 1, 1]       # bounds -- [x_min y_min; x_max y_max]
+       )
 ```
 
 Mesh for a square `[-1, 1]×[-1, 1]` with a circular hole
 ```
 julia> d = setdiff(
-            Rect(-1, 1, -1, 1)),
-            Circle([0.0, 0.0], 1/2)
-        );
+           Rect(-1, 1, -1, 1)),
+           Circle([0.0, 0.0], 1/2)
+       );
 julia> mesh = Mesh(
-        d,                  # d -- signed distance function
-        pt -> 1,            # h -- element size function
-        0.05,               # h0 -- initial element size
-        [-1 -1; 1, 1]       # bounds -- [x_min y_min; x_max y_max]
-    )
-Mesh(Matrix[... ], Matrix[...])
+           d,                  # d -- signed distance function
+           pt -> 1,            # h -- element size function
+           0.05,               # h0 -- initial element size
+           [-1 -1; 1, 1]       # bounds -- [x_min y_min; x_max y_max]
+       );
 ```
 """
 module SimpleDistMesh
